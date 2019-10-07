9d265977394a0d6fc3c93f4fee346dd8da101b90
Bayo Opadeyi <bayokrapht@gmail.com>
2019-10-07 17:14:45 +0100

    new fields for ranger and pcp

--- careweb/client/models.py
+++ careweb/client/models.py
@@ -1,9 +1,28 @@
 from django.db import models
 
 
+class LGA(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    lga = models.ForeignKey(LGA, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
 class CareProvider(models.Model):
     code_no = models.CharField(max_length=20)
     name = models.CharField(max_length=200)
+    address = models.TextField(null=True)
+    phone1 = models.CharField(max_length=100, null=True, blank=True)
+    phone2 = models.CharField(max_length=100, null=True, blank=True)
+    lga = models.ForeignKey(LGA, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
@@ -13,6 +32,7 @@ class Ranger(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     phone = models.CharField(max_length=50)
+    location = models.ForeignKey(Location, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return '{} {}'.format(self.first_name, self.last_name)
