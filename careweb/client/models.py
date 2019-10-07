d0dffe8070d57bf4044c680d492c65f1511591ab
Bayo Opadeyi <bayokrapht@gmail.com>
2019-10-07 13:50:04 +0100

    finished models

--- careweb/client/models.py
+++ careweb/client/models.py
@@ -1,6 +1,48 @@
 from django.db import models
 
 
+class CareProvider(models.Model):
+    code_no = models.CharField(max_length=20)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Ranger(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=50)
+
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+
+class HMO(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Dependant(models.Model):
+    SPOUSE = 0
+    DAUGHTER = 1
+    SON = 2
+    OTHERS = 3
+    DESIGNATIONS = enumerate(('Spouse', 'Daughter', 'Son', 'Others'))
+
+    surname = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, blank=True)
+    dob = models.DateField(null=True, blank=True)
+    designation = models.PositiveIntegerField(choices=DESIGNATIONS)
+    pcp = models.ForeignKey(CareProvider, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.surname
+
+
 class Client(models.Model):
     FEMALE = 0
     MALE = 1
@@ -10,6 +52,23 @@ class Client(models.Model):
     MARRIED = 1
     DIVORCED = 2
     MARITAL_STATUSES = enumerate(('Single', 'Married', 'Divorced'))
+
+    LASHMA = 0
+    LASHMA_QUALITY_LIFE = 1
+    PACKAGE_OPTIONS = enumerate(('LASHMA', 'LASHMA QUALITY LIFE'))
+
+    WEEKLY = 0
+    MONTHLY = 1
+    QUARTERLY = 2
+    ANNUALLY = 3
+    PAYMENT_OPTIONS = enumerate(('Weekly', 'Monthly', 'Quarterly', 'Annually'))
+
+    TRANSFER = 0
+    EWALLET = 1
+    CHEQUE = 2
+    BANK_DEPOSIT = 3
+    PAYMENT_INSTRUMENTS = enumerate(
+        ('Transfer', 'E-Wallet', 'Cheque', 'Bank Deposit'))
 
     surname = models.CharField(max_length=100)
     first_name = models.CharField(max_length=100)
@@ -25,3 +84,21 @@ class Client(models.Model):
     phone_no = models.CharField(max_length=50, blank=True)
     whatsapp_no = models.CharField(max_length=50, blank=True)
     email = models.CharField(max_length=50, blank=True)
+    pcp = models.ForeignKey(CareProvider, null=True, on_delete=models.SET_NULL)
+    ranger = models.ForeignKey(Ranger, null=True, on_delete=models.SET_NULL)
+    home_address = models.TextField(blank=True)
+    occupation = models.CharField(max_length=200, blank=True)
+    company = models.CharField(max_length=200, blank=True)
+    office_address = models.TextField(blank=True)
+    hmo = models.ForeignKey(HMO, null=True, on_delete=models.SET_NULL)
+    informal_sector_group = models.CharField(max_length=200, null=True)
+    package_option = models.PositiveIntegerField(
+        choices=PACKAGE_OPTIONS, null=True)
+    payment_option = models.PositiveIntegerField(
+        choices=PAYMENT_OPTIONS, null=True)
+    payment_instrument = models.PositiveIntegerField(
+        choices=PAYMENT_INSTRUMENTS, null=True)
+    dependants = models.ManyToManyField(Dependant, blank=True)
+
+    def __str__(self):
+        return self.surname
