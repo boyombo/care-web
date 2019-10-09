4fd372d6d88761e7cab92f5b1efe021d58fd5d19
Bayo Opadeyi <bayokrapht@gmail.com>
2019-10-09 05:15:05 +0100

    added client list to login payload

--- careweb/core/views.py
+++ careweb/core/views.py
@@ -6,6 +6,7 @@ from core.forms import LoginForm
 from ranger.models import Ranger
 from provider.models import CareProvider
 from location.models import LGA
+from client.models import Client
 
 
 @csrf_exempt
@@ -45,6 +46,17 @@ def login_agent(request):
                             'name': lga.name
                         } for lga in LGA.objects.all()
                     ]
+                    clients = [
+                        {
+                            'id': client.id,
+                            'surname': client.surname,
+                            'first_name': client.first_name,
+                            'middle_name': client.middle_name,
+                            'phone_no': client.phone_no,
+                            'whatsapp_no': client.whatsapp_no,
+                            'email': client.email,
+                        } for client in Client.objects.filter(ranger=agent)
+                    ]
 
                     return JsonResponse(
                         {
@@ -57,7 +69,8 @@ def login_agent(request):
                                 'lga_id': agent.lga.id,
                             },
                             'providers': providers,
-                            'lgas': lgas
+                            'lgas': lgas,
+                            'clients': clients
                         }
                     )
         return JsonResponse(
