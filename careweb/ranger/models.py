95ae9258cc93539c70f5e71b56f1052440745221
Bayo Opadeyi <bayokrapht@gmail.com>
2019-10-21 13:02:05 +0100

    ranger balance

--- careweb/ranger/models.py
+++ careweb/ranger/models.py
@@ -11,6 +11,7 @@ class Ranger(models.Model):
     last_name = models.CharField(max_length=200)
     phone = models.CharField(max_length=50)
     lga = models.ForeignKey(LGA, null=True, on_delete=models.SET_NULL)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
         return '{} {}'.format(self.first_name, self.last_name)
