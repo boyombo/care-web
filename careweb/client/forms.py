e4a570da0a8b902d779560707b1ff226e2d7fb9c
Bayo Opadeyi <bayokrapht@gmail.com>
2019-10-31 14:01:16 +0100

    validation of emails

--- careweb/client/forms.py
+++ careweb/client/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 
 from client.models import Client, Association, Dependant
 
@@ -9,7 +10,17 @@ class RegForm(forms.ModelForm):
 
     class Meta:
         model = Client
-        fields = ['email', 'first_name', 'surname']
+        fields = ["email", "first_name", "surname"]
+
+    def clean_email(self):
+        if "email" in self.cleaned_data:
+            email = self.cleaned_data["email"]
+            try:
+                User.objects.get(username=email)
+            except User.DoesNotExist:
+                return email
+            else:
+                raise forms.ValidationError("The email has already been used")
 
 
 class LoginForm(forms.Form):
@@ -17,50 +28,55 @@ class LoginForm(forms.Form):
     password = forms.CharField(max_length=100, widget=forms.PasswordInput)
 
     def clean(self):
-        if 'username' in self.cleaned_data and 'password' in self.cleaned_data:
-            username = self.cleaned_data['username']
-            password = self.cleaned_data['password']
+        if "username" in self.cleaned_data and "password" in self.cleaned_data:
+            username = self.cleaned_data["username"]
+            password = self.cleaned_data["password"]
             user = authenticate(username=username, password=password)
             if user is None:
-                raise forms.ValidationError('Wrong username/password')
+                raise forms.ValidationError("Wrong username/password")
             try:
                 Client.objects.get(user=user)
             except Client.DoesNotExist:
                 raise forms.ValidationError(
-                    'Your account is not registered as a client')
+                    "Your account is not registered as a client"
+                )
             return self.cleaned_data
 
 
 class PersonalInfoForm(forms.ModelForm):
-    dob = forms.DateField(
-        widget=forms.SelectDateWidget(years=range(1950, 2001)))
+    dob = forms.DateField(widget=forms.SelectDateWidget(years=range(1950, 2001)))
 
     class Meta:
         model = Client
-        fields = ['first_name', 'surname', 'middle_name', 'dob',
-                  'sex', 'marital_status']
+        fields = [
+            "first_name",
+            "surname",
+            "middle_name",
+            "dob",
+            "sex",
+            "marital_status",
+        ]
 
 
 class AssociationsForm(forms.ModelForm):
     associations = forms.ModelMultipleChoiceField(
-        queryset=Association.objects.all(),
-        widget=forms.CheckboxSelectMultiple())
+        queryset=Association.objects.all(), widget=forms.CheckboxSelectMultiple()
+    )
 
     class Meta:
         model = Client
-        fields = ['associations']
+        fields = ["associations"]
 
 
 class DependantForm(forms.ModelForm):
-    dob = forms.DateField(
-        widget=forms.SelectDateWidget(years=range(1970, 2019)))
+    dob = forms.DateField(widget=forms.SelectDateWidget(years=range(1970, 2019)))
 
     class Meta:
         model = Dependant
-        exclude = ['primary']
+        exclude = ["primary"]
 
 
 class PhotoForm(forms.ModelForm):
     class Meta:
         model = Client
-        fields = ['photo']
+        fields = ["photo"]
