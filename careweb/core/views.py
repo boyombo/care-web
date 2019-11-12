3c569d2f0b8b1e7a15105b57292f392c7d95e4f1
Bayo Opadeyi <bayokrapht@gmail.com>
2019-11-12 06:10:58 +0100

    add username to login response

--- careweb/core/views.py
+++ careweb/core/views.py
@@ -10,66 +10,52 @@ from location.models import LGA
 
 @csrf_exempt
 def login_agent(request):
-    if request.method == 'POST':
-        #import pdb;pdb.set_trace()
+    if request.method == "POST":
+        # import pdb;pdb.set_trace()
         form = LoginForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
             usr = authenticate(username=username, password=password)
             if usr is not None:
                 try:
                     agent = Ranger.objects.get(user=usr)
                 except Ranger.DoesNotExist:
                     return JsonResponse(
-                        {
-                            'error': 'The user is not an ranger',
-                            'success': False
-                        }
+                        {"error": "The user is not an ranger", "success": False}
                     )
                 else:
                     providers = [
                         {
-                            'id': prov.id,
-                            'name': prov.name,
-                            'address': prov.address,
-                            'phone1': prov.phone1,
-                            'phone2': prov.phone2,
-                            'lga_name': prov.lga.name,
-                            'lga_id': prov.lga.id,
-                        } for prov in CareProvider.objects.all()
+                            "id": prov.id,
+                            "name": prov.name,
+                            "address": prov.address,
+                            "phone1": prov.phone1,
+                            "phone2": prov.phone2,
+                            "lga_name": prov.lga.name,
+                            "lga_id": prov.lga.id,
+                        }
+                        for prov in CareProvider.objects.all()
                     ]
                     lgas = [
-                        {
-                            'id': lga.id,
-                            'name': lga.name
-                        } for lga in LGA.objects.all()
+                        {"id": lga.id, "name": lga.name} for lga in LGA.objects.all()
                     ]
 
                     return JsonResponse(
                         {
-                            'success': True,
-                            'ranger': {
-                                'id': agent.id,
-                                'first_name': agent.first_name,
-                                'last_name': agent.last_name,
-                                'phone': agent.phone,
-                                'lga_name': agent.lga.name,
-                                'lga_id': agent.lga.id,
+                            "success": True,
+                            "ranger": {
+                                "id": agent.id,
+                                "username": agent.user.username,
+                                "first_name": agent.first_name,
+                                "last_name": agent.last_name,
+                                "phone": agent.phone,
+                                "lga_name": agent.lga.name,
+                                "lga_id": agent.lga.id,
                             },
-                            'providers': providers,
-                            'lgas': lgas,
+                            "providers": providers,
+                            "lgas": lgas,
                         }
                     )
-        return JsonResponse(
-            {
-                'error': 'Wrong credentials',
-                'success': False
-            }
-        )
-    return JsonResponse(
-        {
-            'error': 'Bad Request',
-            'success': False
-        }
-    )
+        return JsonResponse({"error": "Wrong credentials", "success": False})
+    return JsonResponse({"error": "Bad Request", "success": False})
