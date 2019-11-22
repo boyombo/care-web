baf1449442e77d5502e17ff0dabbf357ff03d302
Bayo Opadeyi <bayokrapht@gmail.com>
2019-11-22 02:52:46 +0100

    reset password

--- careweb/careweb/urls.py
+++ careweb/careweb/urls.py
@@ -3,7 +3,9 @@
 # from django.contrib import admin
 from baton.autodiscover import admin
 from django.conf import settings
-from django.contrib.auth.views import LoginView, LogoutView
+
+# from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 from django.urls import path, include
 from django.views.generic import TemplateView
@@ -25,8 +27,30 @@ urlpatterns = [
 
 
 urlpatterns += [
-    # path('accounts/login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path("accounts/logout/", LogoutView.as_view(next_page="/"), name="logout"),
+    # path('accounts/login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path(
+        "accounts/logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout"
+    ),
+    path(
+        "accounts/password-reset/",
+        auth_views.PasswordResetView.as_view(from_email="noreply@futurecare.ng"),
+        name="password-reset",
+    ),
+    path(
+        "accounts/password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "accounts/reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(post_reset_login=True),
+        name="password_reset_confirm",
+    ),
+    path(
+        "accounts/reset/complete/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
 ]
 
 admin.site.site_header = "Futurecare"
@@ -34,3 +58,8 @@ admin.site.site_header = "Futurecare"
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [path("__debug__/", include(debug_toolbar.urls)),] + urlpatterns
