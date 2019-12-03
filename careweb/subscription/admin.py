d0eba4875d95e7e326c072802fb8bb921d6fc54f
Bayo Opadeyi <bayokrapht@gmail.com>
2019-12-03 12:29:29 +0100

    agent subscription

--- careweb/subscription/admin.py
+++ careweb/subscription/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django import forms
 
 from subscription.models import Subscription, SubscriptionPayment
 
@@ -17,6 +18,12 @@ class SubscriptionAdmin(admin.ModelAdmin):
     list_filter = ["active"]
 
 
+class PaymentForm(forms.ModelForm):
+    class Meta:
+        model = SubscriptionPayment
+        fields = ["client", "amount"]
+
+
 @admin.register(SubscriptionPayment)
 class SubscriptionPaymentAdmin(admin.ModelAdmin):
     list_display = [
@@ -29,3 +36,11 @@ class SubscriptionPaymentAdmin(admin.ModelAdmin):
     ]
     search_fields = ["client__user__username", "client__first_name"]
     list_filter = ["status"]
+    autocomplete_fields = ["client"]
+    form = PaymentForm
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(ranger__user=request.user)
