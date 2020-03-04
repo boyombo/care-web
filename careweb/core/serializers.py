10149e18df26fc130d2fb844b7cfd15193c6db90
yusufoguntola <yusuf.oguntola@gmail.com>
2020-03-04 12:47:38 +0100

    Fixed lga_id issue with provider serializer

--- careweb/core/serializers.py
+++ careweb/core/serializers.py
@@ -6,12 +6,11 @@ from core.models import Plan, PlanRate
 class PlanSerializer(serializers.ModelSerializer):
     class Meta:
         model = Plan
-        fields = ['name', 'code', 'id', 'size']
+        fields = ['name', 'code', 'id', 'size', 'has_extra', 'family_inclusive']
 
 
 class PlanRateSerializer(serializers.ModelSerializer):
-    plan = PlanSerializer()
 
     class Meta:
         model = PlanRate
-        fields = ['plan', 'payment_cycle', 'rate', 'extra_rate']
+        fields = ['plan_id', 'id', 'payment_cycle', 'rate', 'extra_rate']
--- careweb/provider/serializers.py
+++ careweb/provider/serializers.py
@@ -1,5 +1,6 @@
 from hashid_field.rest import HashidSerializerCharField
 from rest_framework import serializers
+from rest_framework.relations import PrimaryKeyRelatedField
 
 from location.serializers import LGASerializer
 from provider.models import CareProvider
@@ -7,8 +8,9 @@ from provider.models import CareProvider
 
 class ProviderSerializer(serializers.ModelSerializer):
     id = HashidSerializerCharField(source_field="provider.CareProvider.id", read_only=True)
-    lga = LGASerializer()
+    lga_id = PrimaryKeyRelatedField(pk_field=HashidSerializerCharField(source_field="location.LGA.id"), read_only=True)
+    # lga = LGASerializer()
 
     class Meta:
         model = CareProvider
-        fields = ['id', 'code_no', 'name', 'address', 'phone1', 'phone2', 'lga_id', 'lga']
+        fields = ['id', 'code_no', 'name', 'address', 'phone1', 'phone2', 'lga_id']
