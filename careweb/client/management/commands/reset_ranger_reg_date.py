653a00c7d73f71636fe2f347d36506a71335614d
yusufoguntola <yusuf.oguntola@gmail.com>
2020-03-24 06:51:50 +0100

    Created management command for setting reg date of rangers

---
+++ careweb/client/management/commands/reset_ranger_reg_date.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from django.core.management.base import BaseCommand
+
+from ranger.models import Ranger
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('--date')
+
+    def handle(self, *args, **options):
+        updated = 0
+        current = datetime.strptime("03/23/2020", "%m/%d/%Y")
+        date = options.get("date")
+        if date:
+            default = datetime.strptime(date, "%m/%d/%Y")
+        else:
+            default = datetime.strptime("03/02/2020", "%m/%d/%Y")
+        for ranger in Ranger.objects.all():
+            if ranger.created.date() == current:
+                ranger.created = default
+                ranger.save()
+                updated += 1
+        print("%s rangers updated" % updated)
