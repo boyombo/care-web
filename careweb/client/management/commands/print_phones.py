f2de754cdc3482027c0fb41b9f7aa24c5d86b321
yusufoguntola <yusuf.oguntola@gmail.com>
2020-05-22 15:18:41 +0100

    Added phone command

---
+++ careweb/client/management/commands/print_phones.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+
+from client.models import Client
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        print("Initialized")
+        phones = []
+        for client in Client.objects.all():
+            phone = client.phone_no
+            phone_number = phone.replace("0", "234", 1) if str(phone).startswith("0") else phone
+            phones.append(phone_number)
+
+        print(",".join(phones))
