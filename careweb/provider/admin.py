21d610c1efbf0de210bcfc759a8ae664a2d71f22
yusufoguntola <yusuf.oguntola@gmail.com>
2020-06-22 09:38:31 +0100

    Added email sending capability for provider reg

--- careweb/provider/admin.py
+++ careweb/provider/admin.py
@@ -4,6 +4,7 @@ from simple_history.admin import SimpleHistoryAdmin
 from constance import config
 
 from careweb.utils import HashIdFieldAdminMixin
+from core.utils import send_email
 from provider.forms import CareProviderFormAdmin
 from provider.models import CareProvider
 from provider.util import is_valid_provider_mail
@@ -25,11 +26,16 @@ class CareProviderAdmin(HashIdFieldAdminMixin, SimpleHistoryAdmin):
         email = obj.email
         if email and is_valid_provider_mail(obj.id, email):
             if not User.objects.filter(username=email).exists():
-                print("Provider does not currently have a user attached")
+                # print("Provider does not currently have a user attached")
                 password = config.CLIENT_DEFAULT_PASSWORD
                 user = User.objects.create_user(email, email=email, password=password, first_name=obj.name)
                 user.user_permissions.add(Permission.objects.get(codename='is_provider'))
                 user.save()
                 obj.uses_default_password = True
-                print("User created for the client")
+                try:
+                    context = {"name": obj.name, "username": email, "password": password}
+                    send_email(obj.email, "provider_welcome_email", context)
+                except:
+                    pass
+                # print("User created for the client")
         super(CareProviderAdmin, self).save_model(request, obj, form, change)
